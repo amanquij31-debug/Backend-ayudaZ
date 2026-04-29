983d3de52f08a323527b21019609f773b79c7785
Ivan <Ivansaturnomirana@gmail.com>
2026-04-28 23:34:23 -0500

    Proyecto limpio

---
+++ src/test/java/com/ayudaz/ayudaz_backend/AyudazBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayudaz.ayudaz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyudazBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

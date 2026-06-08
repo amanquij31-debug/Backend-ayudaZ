a7a1a1fe970b5ddfac26d930a836132da6a0ec10
Mnz6 <marianokids512@gmail.com>
2026-06-08 17:55:41 -0500

    Delete src/main/java/com/ayudaz/backend/repository directory

--- src/main/java/com/ayudaz/backend/repository/UrgenciaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ayudaz.backend.repository;
-
-import com.ayudaz.backend.model.Urgencia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UrgenciaRepository extends JpaRepository<Urgencia, Long> {
-}
